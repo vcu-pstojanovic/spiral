a2d0086a0dbc8dd705b2246a0e5d0966f6af4cf1
vcu-pstojanovic <78378662+vcu-pstojanovic@users.noreply.github.com>
2021-04-09 09:29:54 -0400

    Black fix

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,4 @@
 def spiralize(number):
-  # ulam spiral https://www.educative.io/edpresso/how-to-solve-the-number-spiral-diagonals-problem
-  n = (number - 1) /2
-  return (3 + 2 * n * ( 8 * n * n + 15 * n +13)) /3
+    # ulam spiral https://www.educative.io/edpresso/how-to-solve-the-number-spiral-diagonals-problem
+    n = (number - 1) / 2
+    return (3 + 2 * n * (8 * n * n + 15 * n + 13)) / 3
